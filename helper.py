a29182fdba136608fffb0827184820b531bd2071
Anna Petrasova <kratochanna@gmail.com>
2019-11-20 15:14:36 -0500

    fix georeferncing, add postprocessing

--- helper.py
+++ helper.py
@@ -4,6 +4,7 @@ import os
 import sys
 import subprocess
 import folium
+import json
 
 
 def initialize_GRASS_notebook(binary, grassdata, location, mapset):
@@ -40,6 +41,7 @@ def adjust_futures_colors(raster):
     gs.write_command('r.colors', map=new_raster, stdin=color, rules='-')
     return new_raster
 
+
 def show(raster):
     from IPython.display import Image
     import grass.script as gs
@@ -48,23 +50,26 @@ def show(raster):
     gs.run_command('d.vect', map='counties', fill_color='none', width=2)
     return Image("map.png")
 
-def show_interactively(raster):
+
+def show_interactively(raster, opacity=0.8):
     import grass.script as gs
-    region = gs.parse_command('g.region', flags='lcg')
-    raster = adjust_futures_colors(raster)
     gs.run_command('r.out.gdal', input=raster, output=raster + '_spm.tif', type='Byte')
     subprocess.call(['gdalwarp', '-t_srs', 'EPSG:3857',  raster + '_spm.tif', raster + '_merc.tif', '-overwrite'])
     subprocess.call(['gdal_translate', '-of', 'png', raster + '_merc.tif', raster + '_merc.png'])
-    minlat = min(float(region['se_lat']), float(region['sw_lat']))
-    maxlat = max(float(region['ne_lat']), float(region['nw_lat']))
-    minlon = min(float(region['sw_long']), float(region['nw_long']))
-    maxlon = min(float(region['se_long']), float(region['ne_long']))
-    m = folium.Map(location=[35.87008220, -78.78423295])
+    info = subprocess.check_output(['gdalinfo', '-json', '-noct', '-nomd', raster + '_merc.png'], universal_newlines=True)
+    coors = json.loads(info)['wgs84Extent']['coordinates'][0]
+    lon = [pt[0] for pt in coors]
+    lat = [pt[1] for pt in coors]
+    minlat = min(lat)
+    minlon = min(lon)
+    maxlat = max(lat)
+    maxlon = max(lon)
+    m = folium.Map(location=[(maxlat + minlat) / 2, (maxlon + minlon) / 2])
     img = folium.raster_layers.ImageOverlay(
             name=raster,
             image=raster + '_merc.png',
             bounds=[[minlat, minlon], [maxlat, maxlon]],
-            opacity=1,
+            opacity=opacity,
             interactive=True,
             cross_origin=False,
         )
